68137ddd9bfb5549d462a29aaafcd01b2926cb36
Roberto Ciatti <gekorob@users.noreply.github.com>
2020-09-29 23:07:05 +0200

    Add screenlayouts

---
+++ screenlayout/home_docking.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 1306x1080 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-1-1 --mode 1920x1080 --pos 1146x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --mode 1920x1080 --pos 0x60 --rotate right
---
+++ screenlayout/midres_alone.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 152x1080 --rotate normal --output DP-1-2 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off
