6487d1e0a52d180ad6c9a6801be6c254bfff50fd
gekorob <gekorob@users.noreply.github.com>
2020-10-07 17:10:44 +0200

    Fix home display outputs

--- screenlayout/midres_alone.sh
+++ screenlayout/midres_alone.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1600x900 --pos 152x1080 --rotate normal --output DP-1-2 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off
+
+xrandr --newmode "1600x900" 146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
+xrandr --addmode eDP-1 1600x900
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
